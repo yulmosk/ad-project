3718892aeeb8e72712b397c843e7aac10028d87c
Yulia Moskaleva <dnsuser@dnss-MacBook-Pro.local>
2021-09-05 11:49:18 +0300

    2.2-2.3

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,32 @@
 <template>
   <v-app>
-  <v-navigation-drawer app></v-navigation-drawer> 
+  <v-navigation-drawer app v-model="drawer">
+    <v-list-item>
+      <v-list-item-content>
+            <v-list-item-title class="title">
+              КИПУ
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              Учебный проект
+            </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider></v-divider>
+    <v-list dense>
+        <v-list-item-group color="primary">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-cake-variant</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Link One</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer> 
   <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
        <v-btn  text><v-icon left>mdi-cake-variant</v-icon>Link One</v-btn>
@@ -19,6 +43,10 @@
 <script>
 
 export default {
-  
+  data() {
+    return {
+      drawer: false
+    }
+  }
 };
 </script>
