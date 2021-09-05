c68ba289a2e5c4656557b547f963216a1321c767
Yulia Moskaleva <dnsuser@dnss-MacBook-Pro.local>
2021-09-05 14:40:23 +0300

    Подключение переменных, валидации и метода onSubmit на экране Login

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -7,23 +7,33 @@
 					Login
 				</v-toolbar>
 				<v-card-text>
-					<v-form>
+					<v-form v-model="valid" ref="form" validation>
 						<v-text-field
 						prepend-icon="mdi-account"
 						name="email"
 						label="Email"
-						type="email"></v-text-field>
+						type="email"
+						v-model="email"
+						:rules="emailRules"
+						></v-text-field>
 						<v-text-field
                         prepend-icon="mdi-lock" 
                         name="password" 
                         label="Password" 
-                        type="password">
+                        type="password"
+                        v-model="password"
+                        :rules="passwordRules"
+                        >
                         </v-text-field>
 					</v-form>
 				</v-card-text>
 				<v-card-actions>
 					<v-spacer></v-spacer>
-					<v-btn color="primary">Login</v-btn>
+					<v-btn 
+					color="primary"
+					@click="onSubmit"
+					:disabled="!valid"
+					>Login</v-btn>
 				</v-card-actions>
 			</v-card>
 		</v-flex>
@@ -36,7 +46,27 @@
 export default {
 	data () { 
 		return {
+			valid: false,
+			email: "",
+			password: "",
+			emailRules: [
+			v => !!v || 'E-mail is required',
+			v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+			],
+			passwordRules: [
+			v => !!v || 'Password is required',
+			v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters'
+			]
 		} 	
+	},
+	methods: {
+		onSubmit(){
+			const user = {
+				email: this.email,
+				password: this.password
+			}
+			console.log(user)
+		}
 	}
 } 
 </script>
