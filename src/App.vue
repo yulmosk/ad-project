f9831853e720099046383d3c2b710888b9c7d7a8
Yulia Moskaleva <dnsuser@dnss-MacBook-Pro.local>
2021-09-05 11:22:37 +0300

    2.1-2.2

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>Прюветь</h1>
+  <v-app>
+  <v-navigation-drawer app></v-navigation-drawer> 
+  <v-app-bar app dark color="primary">
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+       <v-btn  text><v-icon left>mdi-cake-variant</v-icon>Link One</v-btn>
+       <v-btn  text>Link Two</v-btn> 
+       <v-btn  text>Link Three</v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+  <v-main>
+    <router-view></router-view> 
+  </v-main>
+  </v-app>
 </template>
 
 <script>
