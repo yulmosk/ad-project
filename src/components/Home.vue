36f2c7bb1716cafce92213cffc9b61f45ebdf08e
Yulia Moskaleva <dnsuser@dnss-MacBook-Pro.local>
2021-09-05 15:35:34 +0300

    Подключение данных к главной странице

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,28 +4,39 @@
 		<v-layout row> 
 			<v-flex xs12>
 				<v-carousel>
-					<v-carousel-item></v-carousel-item>
+					<v-carousel-item
+						v-for="ad in ads"
+						:key="ad.id"
+						:src="ad.src"
+					>
+					<div class="ad-link">
+						<v-btn class="error" :to="'/ad/' + ad.id">{{ ad.title }}</v-btn>
+					</div>
+					</v-carousel-item>
 				</v-carousel>
 			</v-flex>
 		</v-layout> 
 	</v-container>
-	<v-container>
-		<v-layout row> 
-			<v-flex xs12>
+	<v-container grid-list-lg>
+		<v-layout row wrap> 
+			<v-flex xs12 sm6 md4
+			v-for="ad in ads"
+			:key="ad.id">
 				<v-card>
 					<v-img
+					:src="ad.src"
 					height="200px"
 					></v-img>
 					<v-card-title primary-title>
 					<div>
 						<h3 class="headline mb0">
-						Здесь будет заголовок</h3>
-						<div>Здесь будет описание</div>
+						{{ad.title}}</h3>
+						<div>{{ad.desc}}</div>
 					</div>
 					</v-card-title>
 					<v-card-actions>
 					<v-spacer></v-spacer>
-					<v-btn text>Open</v-btn>
+					<v-btn text :to="'/ad/' + ad.id">Open</v-btn>
 					<v-btn raised color="primary">
 						Buy
 					</v-btn>
@@ -75,3 +86,16 @@ export default {
 	}
 } 
 </script>
+
+<style scoped>
+	.ad-link {
+		position:absolute;
+		bottom: 50px;
+		left:50%;
+		background: rgb(0,0,0,0.5);
+		transform: translate(-50%,0);
+		padding: 5px 15px;
+		border-top-right-radius: 5px;
+		border-top-left-radius: 5px;
+	}
+</style>
