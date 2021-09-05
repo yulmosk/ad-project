c9805c44c71fde9e1f384389a0bb1086294a4fc7
Yulia Moskaleva <dnsuser@dnss-MacBook-Pro.local>
2021-09-05 13:51:54 +0300

    Верстка страницы логина 3.1

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,13 +1,38 @@
 <template> 
-	<v-container>
-		<v-layout row> 
-		<v-flex xs12>
-				<h1>Login</h1> 
-			</v-flex>
+	<v-container fluid fill-height>
+		<v-layout align-center justify-center> 
+		<v-flex xs12 sm8 md8>
+			<v-card class="elevetion-12">
+				<v-toolbar dark color="primary">
+					Login
+				</v-toolbar>
+				<v-card-text>
+					<v-form>
+						<v-text-field
+						prepend-icon="mdi-account"
+						name="email"
+						label="Email"
+						type="email"></v-text-field>
+						<v-text-field
+                        prepend-icon="mdi-lock" 
+                        name="password" 
+                        label="Password" 
+                        type="password">
+                        </v-text-field>
+					</v-form>
+				</v-card-text>
+				<v-card-actions>
+					<v-spacer></v-spacer>
+					<v-btn color="primary">Login</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-flex>
 		</v-layout> 
 	</v-container>
 </template>
+
 <script>
+
 export default {
 	data () { 
 		return {
