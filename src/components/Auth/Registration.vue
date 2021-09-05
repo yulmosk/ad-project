e5164ebbc0a064e9bb5dca2859addc07dac7a9b1
Yulia Moskaleva <dnsuser@dnss-MacBook-Pro.local>
2021-09-05 14:49:32 +0300

    Экран регистрации

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,17 +1,86 @@
 <template> 
-	<v-container>
-		<v-layout row> 
-		<v-flex xs12>
-				<h1>Registration</h1> 
-			</v-flex>
+	<v-container fluid fill-height>
+		<v-layout align-center justify-center> 
+		<v-flex xs12 sm8 md8>
+			<v-card class="elevetion-12">
+				<v-toolbar dark color="primary">
+					Registration
+				</v-toolbar>
+				<v-card-text>
+					<v-form v-model="valid" ref="form" validation>
+						<v-text-field
+						prepend-icon="mdi-account"
+						name="email"
+						label="Email"
+						type="email"
+						v-model="email"
+						:rules="emailRules"
+						></v-text-field>
+						<v-text-field
+                        prepend-icon="mdi-lock" 
+                        name="password" 
+                        label="Password" 
+                        type="password"
+                        v-model="password"
+                        :rules="passwordRules"
+                        >
+                        </v-text-field>
+                        <v-text-field
+                        prepend-icon="mdi-lock" 
+                        name="confirm-password" 
+                        label="Confirm Password" 
+                        type="password"
+                        v-model="confirmPassword"
+                        :rules="confirmPasswordRules"
+                        >
+                        </v-text-field>
+					</v-form>
+				</v-card-text>
+				<v-card-actions>
+					<v-spacer></v-spacer>
+					<v-btn 
+					color="primary"
+					@click="onSubmit"
+					:disabled="!valid"
+					>Create Account</v-btn>
+				</v-card-actions>
+			</v-card>
+		</v-flex>
 		</v-layout> 
 	</v-container>
 </template>
+
 <script>
+
 export default {
 	data () { 
 		return {
+			valid: false,
+			email: "",
+			password: "",
+			confirmPassword: "",
+			emailRules: [
+			v => !!v || 'E-mail is required',
+			v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+			],
+			passwordRules: [
+			v => !!v || 'Password is required',
+			v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters'
+			],
+			confirmPasswordRules: [
+			v => !!v || 'Password is required',
+			v => v === this.password || 'Password should match'
+			]
 		} 	
+	},
+	methods: {
+		onSubmit(){
+			const user = {
+				email: this.email,
+				password: this.password
+			}
+			console.log(user)
+		}
 	}
 } 
 </script>
