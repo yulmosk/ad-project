90bf34ad7d0f4fc4949f25bb328a05b63433f1fd
Yulia Moskaleva <dnsuser@dnss-MacBook-Pro.local>
2021-10-20 09:01:20 +0300

    Верстка экрана Orders

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,29 @@
 <template> 
 	<v-container>
 		<v-layout row> 
-		<v-flex xs12>
-				<h1>Orders</h1> 
+		<v-flex xs12 sm6 offset-sm3>
+					<h1 class="text--secondary mb-3 mt-3">Orders</h1>
+	<v-list
+      subheader
+      two-line
+      flat>      
+	<v-list-item>
+	<v-list-item-action>
+	<v-checkbox
+		:input-value="active"
+		color="primary"
+	></v-checkbox>
+	</v-list-item-action>
+
+	<v-list-item-content>
+	<v-list-item-title>Notifications</v-list-item-title>
+    <v-list-item-subtitle>Allow notifications</v-list-item-subtitle>
+    </v-list-item-content>
+    <v-list-item-action>
+		<v-btn class="primary">Open</v-btn>
+    </v-list-item-action>
+    </v-list-item>
+</v-list>
 			</v-flex>
 		</v-layout> 
 	</v-container>
