43c976b5005e63c051f4c6762e7fa4940fb30e4d
Yulia Moskaleva <dnsuser@dnss-MacBook-Pro.local>
2021-10-21 12:54:22 +0300

    Тестовые данные для заказов

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -2,25 +2,27 @@
 	<v-container>
 		<v-layout row> 
 		<v-flex xs12 sm6 offset-sm3>
-					<h1 class="text--secondary mb-3 mt-3">Orders</h1>
+			<h1 class="text--secondary mb-3 mt-3">Orders</h1>
 	<v-list
       subheader
       two-line
       flat>      
-	<v-list-item>
+	<v-list-item v-for="order in orders" :key="order.id">
 	<v-list-item-action>
 	<v-checkbox
-		:input-value="active"
+		:input-value="order.done"
 		color="primary"
 	></v-checkbox>
 	</v-list-item-action>
 
 	<v-list-item-content>
-	<v-list-item-title>Notifications</v-list-item-title>
-    <v-list-item-subtitle>Allow notifications</v-list-item-subtitle>
+	<v-list-item-title>{{ order.phone }}</v-list-item-title>
+    <v-list-item-subtitle>{{ order.name }}</v-list-item-subtitle>
     </v-list-item-content>
     <v-list-item-action>
-		<v-btn class="primary">Open</v-btn>
+		<v-btn class="primary"
+		:to="'/ad/' + order.adId"
+		>Open</v-btn>
     </v-list-item-action>
     </v-list-item>
 </v-list>
@@ -32,6 +34,30 @@
 export default {
 	data () { 
 		return {
+			orders:[
+				{
+					id:"123",
+					name:"Kostya",
+					phone:"+7(978)000-00-05",
+					adId:"1",
+					done:true
+				},
+				{
+					id:"124",
+					name:"Masha",
+					phone:"+7(978)333-45-56",
+					adId:"12",
+					done:false
+				},
+				{
+					id:"123",
+					name:"Mila",
+					phone:"+7(978)565-56-23",
+					adId:"23",
+					done:true
+				},
+
+			]
 		} 	
 	}
 } 
